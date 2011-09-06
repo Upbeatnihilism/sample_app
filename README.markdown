a4044dd08dc043ece97ec2cfa0422afbc8d5eee4
Upbeatnihilism <singley.scott@gmail.com>
2011-09-05 22:32:28 -0700

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,6 @@
+# Ruby on Rains Tutorial: Sample Application
+
+This is the sample app for Scott Singley's
+
+[*Ruby on Rails Tutorial: Learn Rails by Example*] (http://railstutorial.org) 
+by [Michale Hartl] (http://michaelhartl.com).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2e35bfd0d7e7a12549b8394765fa6a95c67c7de7ab732e0b79f3aaf00b4025e47a16fe3d6261d7ec78a7840e6670260d3459e9b19d3e98ea134f76523c7f9926'
